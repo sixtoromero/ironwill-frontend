abf1f836380ec12d43d57b9f90a55891bc5f35c1
Sixto José Romero Martínez <sixto.jose@gmail.com>
2021-03-05 18:54:43 -0500

    Se aplican cambios en deportistas

--- src/app/components/shared/history-athlete/history-athlete.component.ts
+++ src/app/components/shared/history-athlete/history-athlete.component.ts
@@ -7,6 +7,7 @@ import { AgendaService } from 'src/app/services/agenda.service';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { finalize } from 'rxjs/operators';
 import { UsuarioModel } from 'src/app/models/usuarios.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-history-athlete',
@@ -18,22 +19,40 @@ export class HistoryAthleteComponent implements OnInit {
   listHistory: AgendaModel[] = [];
   iHistory: AgendaModel = new AgendaModel();
 
+  displayRegisterAgenda: boolean;
+
   @Input() user: UsuarioModel = new UsuarioModel();  
+
+  public myForm: FormGroup  = new FormGroup({});
   
   constructor(
+    private fb: FormBuilder,
     private _general: GeneralService,    
     private confirmationService: ConfirmationService,
-    private ngxService: NgxUiLoaderService,    
+    private ngxService: NgxUiLoaderService,
     private router: Router,
     private _service: AgendaService
-  ) { }
+  ) {
+    
+    this.myForm = fb.group({
+      IdTipoEjercicio: ['', [Validators.required]],
+      Fecha: ['', [Validators.required]],
+      Hora: ['', [Validators.required]]      
+    });
+    
+  }
 
   ngOnInit(): void {    
     this.getHistoryAthlete();
   }
 
-  getHistoryAthlete() {
-    debugger;
+
+
+  get f() {
+    return this.myForm.controls;
+  }
+
+  getHistoryAthlete() {        
     this.ngxService.start();
     this._service.getHistoryAthlete(this.user.IdPerfil)
     .pipe(finalize(() => this.ngxService.stop()))
